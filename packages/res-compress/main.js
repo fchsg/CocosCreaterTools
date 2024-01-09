98ccf8f63759fcaa67b815c6f7ce37efc760e268
fu.chenhao <fu.chenhao@hellogroup.com>
2024-01-09 18:52:02 +0800

    code

--- packages/res-compress/main.js
+++ packages/res-compress/main.js
@@ -16,17 +16,17 @@ module.exports = {
         // 文件移动
         'asset-db:assets-moved': function (event, target) {
             //Editor.log('[Mp3Compress] 文件移动,刷新列表!');
-            Editor.Ipc.sendToPanel('res-compress', 'res-compress:hello', target);
+         //   Editor.Ipc.sendToPanel('res-compress', 'res-compress:hello', target);
         },
         // 文件删除
         'asset-db:assets-deleted': function (event, target) {
             //Editor.log('[Mp3Compress] 文件删除,刷新列表!');
-            Editor.Ipc.sendToPanel('res-compress', 'res-compress:hello', target);
+           // Editor.Ipc.sendToPanel('res-compress', 'res-compress:hello', target);
         },
         // 文件创建
         'asset-db:assets-created': function (event, target) {
             //Editor.log('[Mp3Compress] 文件创建,刷新列表!');
-            Editor.Ipc.sendToPanel('res-compress', 'res-compress:hello', target);
+           // Editor.Ipc.sendToPanel('res-compress', 'res-compress:hello', target);
         },
     },
 };
