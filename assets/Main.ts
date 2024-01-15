fe5b3456fd353fcf0e6ef6b5a7ac8e91d86f61b4
fu.chenhao <fu.chenhao@hellogroup.com>
2024-01-15 14:05:09 +0800

    add test code

---
+++ assets/Main.ts
@@ -0,0 +1,20 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class Main extends cc.Component {
+
+
+
+    // onLoad () {}
+
+    start () {
+        console.log("Hello World 1");
+        console.log("Hello World 2");
+        console.log("Hello World 3");
+        console.log("Hello World 4");
+        console.log("Hello World 5");
+        console.log("Hello World 6");
+    }
+
+    // update (dt) {}
+}
